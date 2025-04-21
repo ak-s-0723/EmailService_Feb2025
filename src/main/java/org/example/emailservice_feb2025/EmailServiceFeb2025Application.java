a18f1b2adba26c1a52e941f5dbdfdd9d58eadf4a
khannaanurag1 <khannaanurag1@gmail.com>
2025-04-21 06:58:11 +0530

    Initial Commit

---
+++ src/main/java/org/example/emailservice_feb2025/EmailServiceFeb2025Application.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_feb2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceFeb2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailServiceFeb2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/emailservice_feb2025/EmailServiceFeb2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_feb2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceFeb2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
